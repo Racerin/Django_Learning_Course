1c8e5fed3203676efb2775f02811480c8d2e7fd6
Racerin <drsbaird@yahoo.com>
2022-03-30 02:23:19 -0400

    feat: Add server side socket functions and cli.

--- scripts/sockets/PARAM.py
+++ scripts/sockets/PARAM.py
@@ -1,2 +1,5 @@
-DEF_TIMEOUT = 5     # timeout duration in seconds
-DEF_WEB_PORT = 80
+DEF_TIMEOUT = 5                 # timeout duration in seconds
+DEF_WEB_PORT = 80               # port number for socket connection to web server
+DEF_WEBSERVER_PORT = 9000       # port number for socket connection for web server
+DEF_SERVER_HOST = 'localhost'   # 
+DEF_SERVER_QUEUE_AMOUNT = 5
--- scripts/sockets/my_socket_lib.py
+++ scripts/sockets/my_socket_lib.py
@@ -8,7 +8,7 @@ class MySocket(socket.socket):
     pass
 
 
-def connect_socket(host : str|int, url : str, port : int=80, print_host_ip : bool =True):
+def connect_socket(host : 'str|int', url : str, port : int=80, print_host_ip : bool =True):
     # Instantiate and config socket
     my_socket = MySocket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to server over the internet
@@ -31,3 +31,48 @@ def connect_socket(host : str|int, url : str, port : int=80, print_host_ip : boo
         print(data.decode(), end="")
 
     my_socket.close()
+
+def create_server():
+    print("Access http://localhost:{}".format(PARAM.DEF_WEBSERVER_PORT))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Configure socket to close properly? WORKS. https://stackoverflow.com/a/6380198/6556801
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        client_socket = None
+
+        server_socket.bind((PARAM.DEF_SERVER_HOST, PARAM.DEF_WEBSERVER_PORT), )
+        server_socket.listen(PARAM.DEF_SERVER_QUEUE_AMOUNT)
+        while True:
+            client_socket, address = server_socket.accept()     # Blocking, waiting for a request
+
+            # Receive the information
+            response_data = client_socket.recv(5000).decode()
+            pieces_of_data = response_data.split("\n")
+            if len(pieces_of_data) > 0:
+                print(pieces_of_data[0])
+
+            # Response details
+            data = """HTTP/1.1 200 OK\r
+            Content-Type: text/html; charset=utf-8\r
+            \r
+            <html><body>Hello world</body></html>\r
+            \r
+            """
+            # alternative
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type: text/html; charset=utf-8\r\n"
+            data += "\r\n"
+            data += "<html><body>Hello World</body></htm    l>\r\n\r\n"
+            client_socket.sendall(data.encode())
+            client_socket.shutdown(socket.SHUT_WR)
+            
+    except KeyboardInterrupt:
+        print("""
+        Shutting down...
+        """)
+    except Exception as e:
+        print("Error:\n")
+        print(e)
+    finally:
+        # client_socket.shutdown(socket.SHUT_WR)
+        server_socket.close()
