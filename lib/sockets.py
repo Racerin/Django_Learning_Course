fe490c126594cfcdb07c684ed28fd2f9a4d0ab23
Darnell Baird <drsbaird@yahoo.com>
2022-12-18 17:54:15 -0400

    feat: Add simple web server

--- lib/sockets.py
+++ lib/sockets.py
@@ -25,3 +25,36 @@ def read_webpage_example(verbose=True) -> str:
     if verbose:
         print(ans)
     return ans
+
+def web_server(ip_address:str='localhost', port:int=9000, def_msg="Hello World", log_func=print) -> None:
+    """ 
+    Create a server and let clients connect to it to request a webpage message.
+     """
+    log_func("Access http://{}:{}".format(ip_address, port) )
+    try:
+        server_socekt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socekt.bind((ip_address, port))
+        server_socekt.listen(5)
+        while(True):
+            (clientsocket, address) = server_socekt.accept()
+
+            rec_data = clientsocket.recv(5000).decode()
+            pieces = rec_data.split("\n")
+            if (len(pieces) > 0):
+                log_func(pieces[0])
+
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type: text/html; charset=utf-8\r\n"
+            data += "\r\n"
+            data += "<html><body><title>{}</title></body></html>\r\n\r\n".format(def_msg)
+            clientsocket.sendall(data.encode())
+            clientsocket.shutdown(socket.SHUT_WR)
+
+    except KeyboardInterrupt:
+        log_func("\nShutting down...\n")
+    except Exception as exc:
+        log_func("Error:\n")
+        log_func(exc)
+    finally:
+        server_socekt.close()
+        log_func("Socket 'http://{}:{}' closed.".format(ip_address, port))
