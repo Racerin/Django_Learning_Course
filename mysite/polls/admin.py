d118890fee1a659c0ceebacbba82783e1c096545
Darnell Baird <drsbaird@yahoo.com>
2022-12-22 01:17:20 -0400

    feat: Add model objects to richly edit objects in database via admin webpage

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Question, Choice
+
 # Register your models here.
+
+# Add the feature in admin webpage to create 'question' object.
+admin.site.register(Question)
+admin.site.register(Choice)
